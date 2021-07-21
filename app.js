4f5631731a8d34241046d69e835c66bc5083fe85
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2021-07-21 15:38:24 -0300

    Implementando os botoes e cmeço do Vue

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 new Vue({
     el:'#app',
     data: {
-        playerLife: 15,
-        monsterLife: 70,
+        running: true,
+        playerLife: 0,
+        monsterLife: 10,
     },
     computed: {
-
+        hasResult(){
+            return this.playerLife == 0 || this.monsterLife == 0
+        }
     },
     methods: {
 
